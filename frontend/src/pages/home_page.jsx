ec73065885e91479ebd6c3706e85bead222fcacb
Sn3s <ycosantos04@gmail.com>
2024-03-13 00:53:35 +0800

    homepage shows all posts

--- frontend/src/pages/home_page.jsx
+++ frontend/src/pages/home_page.jsx
@@ -1,20 +1,36 @@
+import React, { useState, useEffect } from 'react';
 import Post from './Post';
 import './HomePage.css';
 import { Link } from 'react-router-dom';
-import React, {useState, useEffect} from 'react';
-
 
 function HomePage() {
-    
     const [posts, setPosts] = useState([]);
     const [loading, setLoading] = useState(false);
     const [trendingMovies, setTrendingMovies] = useState([]);
     const [error, setError] = useState(null);
 
     useEffect(() => {
+        fetchAllPosts();
         fetchTrendingMovies();
-        fetchFollowedUsersPosts();
     }, []);
+
+    const fetchAllPosts = async () => {
+        setLoading(true);
+        try {
+            const response = await fetch('/api/posts/allfeed');
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const data = await response.json();
+            setPosts(data.feedPosts);
+        } catch (error) {
+            console.error("Error fetching posts:", error);
+            setError("Error fetching posts. Please try again later.");
+        } finally {
+            setLoading(false);
+        }
+    };
+    
 
     const fetchTrendingMovies = async () => {
         try {
@@ -29,62 +45,50 @@ function HomePage() {
         }
     };
 
-    const fetchFollowedUsersPosts = async () => {
-        setLoading(true);
-        try {
-            const response = await fetch('/api/posts/allfeed'); 
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            const data = await response.json();
-            setPosts(data.feedPosts);
-        } catch (error) {
-            console.error("Error fetching posts:", error);
-            setError("Error fetching posts. Please try again later.")
-        } finally {
-            setLoading(false);
-        }
-    };
-    
-    
-    
-    
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
+
     return (
         <>
             <div className='title'>
                 <h1>CineShare</h1>
                 <h2>Home</h2>
             </div>
-            
             <div className='homepage'>
                 <center>
-                <div className='content'>
-                    <div className='userposts'>
+                    <div className='content'>
+                        <div className='userposts'>
                             {loading ? (
                                 <p>Loading posts...</p>
                             ) : (
-                                posts.map(post => (
-                                    <Post key={post._id} post={post} />
+                                posts.map((post, index) => (
+                                    <Post key={index} post={{
+                                        user: post.postedBy ? post.postedBy.username : 'Unknown',
+                                        movieId: post.movieId,
+                                        movie: post.movie,
+                                        content: post.content,
+                                        imageUrl: post.imageUrl,
+                                        timestamp: new Date(post.createdAt).toLocaleDateString() // Adjust the date format as per your needs
+                                    }} />
                                 ))
                             )}
                         </div>
-                    <div className='rightsidebar'>
-                        <div className='sidetitle'>🔥Popular Movies</div>
-                        <div className='sidebarcontent'>
-                        {trendingMovies.map(movie => (
-                            <Link to={`/movie/${movie.id}`} key={movie.id}>
-                            <img
-                                key={movie.id}
-                                className="home_movie-poster"
-                                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                                alt={movie.title}
-                            />
-                            </Link>
-                        ))}
+                        <div className='rightsidebar'>
+                            <div className='sidetitle'>🔥Popular Movies</div>
+                            <div className='sidebarcontent'>
+                                {trendingMovies.map(movie => (
+                                    <Link to={`/movie/${movie.id}`} key={movie.id}>
+                                        <img
+                                            className="home_movie-poster"
+                                            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                                            alt={movie.title}
+                                        />
+                                    </Link>
+                                ))}
+                            </div>
                         </div>
                     </div>
-                    
-                </div>
                 </center>
             </div>
         </>
